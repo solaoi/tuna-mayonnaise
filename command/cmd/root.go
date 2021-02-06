3b85e277143a8427c8722365b553cda120835313
Kodai Aoyama <kodai.aoyama+github@gmail.com>
2021-02-06 18:28:52 +0900

    fix documentation

--- command/cmd/root.go
+++ command/cmd/root.go
@@ -5,16 +5,15 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var cfgFile string
 
 var rootCmd = &cobra.Command{
-	Use:   "tm",
-	Short: "JSON Management System",
-	Long: `TUNA-Mayonnaise is a Low Layer CMS.
-This application is a tool to generate and serve JSON | HTML :)`,
+	Use:   "tuna",
+	Short: "TUNA-Mayonnaise is a CommandLineTool to generate and serve JSON/HTML with Visual Programming.",
+	Long: `TUNA-Mayonnaise is a CommandLineTool to generate and serve JSON/HTML with Visual Programming.
+Complete documentation is available at https://github.com/solaoi/tuna-mayonnaise`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -23,33 +22,5 @@ func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-	}
-}
-
-func init() {
-	cobra.OnInitialize(initConfig)
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tm.yaml)")
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-func initConfig() {
-	if cfgFile != "" {
-		viper.SetConfigFile(cfgFile)
-	} else {
-		home, err := os.UserHomeDir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".tm")
-	}
-
-	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
 }
--- command/go.mod
+++ command/go.mod
@@ -8,5 +8,4 @@ require (
 	github.com/labstack/echo/v4 v4.1.17
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826
 	github.com/spf13/cobra v1.1.1
-	github.com/spf13/viper v1.7.1
 )
